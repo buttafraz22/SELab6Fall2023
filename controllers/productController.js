c2dba91074673024880b05164fbfbb1a1dc41735
buttafraz22 <109384539+buttafraz22@users.noreply.github.com>
2023-10-14 22:05:40 +0500

    update Product API

--- controllers/productController.js
+++ controllers/productController.js
@@ -19,4 +19,26 @@ async function getProduct(req,res){
 
 }
 
-module.exports = { createProduct, getProduct }
+async function updateProduct(req, res) {
+  try {
+    const productId = req.body.id;
+
+    const newProductName = req.body.name;
+
+    const product = await Product.findById({_id: productId});
+
+    if (!product) {
+      return res.status(404).json({ error: "Product not found." });
+    }
+
+    product.name = newProductName;
+
+    await product.save();
+
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
+module.exports = { createProduct, getProduct, updateProduct };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 
-router.post("/createProducts", productController.createProduct);
+router.post("/products/createProduct", productController.createProduct);
 router.get("/products/getProduct", productController.getProduct);
+router.post("/products/updateProduct", productController.updateProduct);
 
 module.exports = router;
